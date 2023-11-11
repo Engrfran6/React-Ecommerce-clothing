4c47dd8d72a61dbac837cdb59bacb29d0079535e
Francis <99579671+Engrfran6@users.noreply.github.com>
2023-11-11 12:38:55 +0100

    created project with vite react and created homepage component and added files for homepage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.css';
+import {HomePage} from './home.component';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+}
---
+++ src/home.component.jsx
@@ -0,0 +1,48 @@
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="card_container">
+        <img src="" alt="" />
+        <div className="card_content">
+          <h1 className="card_title">HATS</h1>
+          <span className="card_subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="card_container">
+        <img src="" alt="" />
+        <div className="card_content">
+          <h1 className="card_title">JACKETS</h1>
+          <span className="card_subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="card_container">
+        <img src="" alt="" />
+        <div className="card_content">
+          <h1 className="card_title">SNEAKERS</h1>
+          <span className="card_subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="card_container">
+        <img src="" alt="" />
+        <div className="card_content">
+          <h1 className="card_title">WOMENS</h1>
+          <span className="card_subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="card_container">
+        <img src="" alt="" />
+        <div className="card_content">
+          <h1 className="card_title">MENS</h1>
+          <span className="card_subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="card_container">
+        <img src="" alt="" />
+        <div className="card_content">
+          <h1 className="card_title">HATS</h1>
+          <span className="card_subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  );
+};
